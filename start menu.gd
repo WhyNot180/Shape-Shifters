ed2fdad3d7fe4b6deee6ea1da44ff365bcd768d6
KRcopy <elandyg1@gmail.com>
2023-02-27 11:02:39 -0500

    added the how to play and changed the multiplayer tab

---
+++ start menu.gd
@@ -0,0 +1,33 @@
+extends Control
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_signleplayer_pressed():
+	get_tree().change_scene() 
+
+func _on_Multiplayer_pressed():
+	get_tree().change_scene("res://PlayerShape.tscn") 
+
+
+func _on_how_to_pressed():
+	get_tree().change_scene("res://howtoplay.tscn")
+
+
+func _on_controls_pressed():
+	get_tree().change_scene.add_child(options)
+
+func _on_Quit_pressed():
+	get_tree().quit()
