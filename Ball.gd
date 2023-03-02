35c3279ca498a5430343d15bd6e294ea5433bcc6
Aydan Boynton <aj01cars@outlook.com>
2023-03-02 11:07:51 -0500

    Export gravity scale vars

--- Ball.gd
+++ Ball.gd
@@ -1,8 +1,13 @@
 extends RigidBody2D
 
-func _on_VisibilityNotifier2D_viewport_exited(viewport):
-	gravity_scale = 10.0
+
+export (float) var increased_gravity_scale = 10.0
+export (float) var normal_gravity_scale = 1.0
 
 
-func _on_VisibilityNotifier2D_viewport_entered(viewport):
-	gravity_scale = 1.0
+func _on_VisibilityNotifier2D_viewport_entered(_viewport):
+	gravity_scale = normal_gravity_scale
+
+
+func _on_VisibilityNotifier2D_viewport_exited(_viewport):
+	gravity_scale = increased_gravity_scale
