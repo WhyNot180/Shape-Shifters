ee79a16319cd380a2319e7f58e6b55d3abc0f44f
KRcopy <elandyg1@gmail.com>
2023-03-10 10:28:52 -0500

    added sound sfx and aligend sever screen

--- Scenes/UI Scripts/Server Menu.gd
+++ Scenes/UI Scripts/Server Menu.gd
@@ -4,17 +4,17 @@ func _ready():
 	pass 
 
 func _on_Join_Server_pressed():
-	if (1024 <= get_node("Join/Port").text.to_int()) && get_node("Join/Port").text.to_int() <= 65535:
-		Lobby.port = get_node("Join/Port").text.to_int()
-		Lobby.ip = get_node("Join/Server IP").text
+	if (1024 <= get_node("Join Menu/Join/Port").text.to_int()) && get_node("Join Menu/Join/Port").text.to_int() <= 65535:
+		Lobby.port = get_node("Join Menu/Join/Port").text.to_int()
+		Lobby.ip = get_node("Join Menu/Join/Server IP").text
 		Lobby.username_text = get_node("Common/Username").text
 		Lobby._on_Join()
 
 
 func _on_Create_Server_pressed():
-	if get_node("Host/Number of players").text != "" && ((1024 <= get_node("Host/Port").text.to_int()) && get_node("Host/Port").text.to_int() <= 65535):
-		Lobby.port = get_node("Host/Port").text.to_int()
-		Lobby.number_of_players = get_node("Host/Number of players").text.to_int()
+	if get_node("Host Menu/Host/Number of players").text != "" && ((1024 <= get_node("Host Menu/Host/Port").text.to_int()) && get_node("Host Menu/Host/Port").text.to_int() <= 65535):
+		Lobby.port = get_node("Host Menu/Host/Port").text.to_int()
+		Lobby.number_of_players = get_node("Host Menu/Host/Number of players").text.to_int()
 		Lobby.username_text = get_node("Common/Username").text
 		Lobby._on_Create_Server()
 
