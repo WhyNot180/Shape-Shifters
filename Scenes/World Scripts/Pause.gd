7086e5f8d133ce076c9262e32dde8ba53e51ce79
KR_copy <65040246+SAOfan@users.noreply.github.com>
2023-03-21 10:45:14 -0400

    changed pause and world

--- Scenes/World Scripts/Pause.gd
+++ Scenes/World Scripts/Pause.gd
@@ -11,14 +11,9 @@ func _ready():
 #	pass
 
 
-<<<<<<< HEAD:LineEdit.gd
-func _on_player_count_text_entered(new_text):
-	pass # Replace with function body.
-=======
 func _unhandled_input(event):
 	if event.is_action_pressed("pause_game"):
 		if get_tree().get_network_unique_id() == 1:
 			Network.disconnect_server()
 		else:
 			Network.disconnect_peer()
->>>>>>> Networking:Scenes/World Scripts/Pause.gd
